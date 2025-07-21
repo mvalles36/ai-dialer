0a7d455b9e9508927a1cfad013a211098c0920f4
Mark Valles <mvalles36@users.noreply.github.com>
2025-07-21 12:23:38 -0500

    Update alert.tsx

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,10 +1,15 @@
+// ExampleAlert.tsx
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert"
 import { Info } from "lucide-react"
 
-<Alert>
-  <Info className="h-4 w-4" />
-  <AlertTitle>Heads up!</AlertTitle>
-  <AlertDescription>
-    You need to verify your email address to continue.
-  </AlertDescription>
-</Alert>
+export default function ExampleAlert() {
+  return (
+    <Alert>
+      <Info className="h-4 w-4" />
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You need to verify your email address to continue.
+      </AlertDescription>
+    </Alert>
+  )
+}
